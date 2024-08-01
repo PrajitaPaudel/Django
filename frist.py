c2aa4f80b24ead32f5cbf067592e97e5b653afd9
prajita <prazup123@gmail.com>
2024-08-01 09:18:52 +0545

    mmmm

---
+++ frist.py
@@ -0,0 +1,27 @@
+select =int(input("Select operations form 1:'add', 2:'sub', 3:'mul', 4 :'divide':"))
+
+
+ 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+ 
+if (select == 1&select == 2&select == 3&select == 4):
+    print("sum is",num1+ num2)
+ 
+ 
+    print(("sub is",num1-num2))
+ 
+ 
+    print("mul is",num1 * num2)
+  
+    print("div is",num1 / num2)
+else:
+    print("Invalid input")
+
+
+
+
+
+
+
+
